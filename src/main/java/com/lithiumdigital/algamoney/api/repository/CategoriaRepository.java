5c78c858344caefd686f15b72d18fee3d9a002c3
gnba <gnba@BRPC003418.gft.com>
2019-07-10 09:19:04 -0300

    Insert: Model, Repository e Controller

---
+++ src/main/java/com/lithiumdigital/algamoney/api/repository/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.lithiumdigital.algamoney.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.lithiumdigital.algamoney.api.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
+
+}
