1bca8e6f343e5d2fb82c725a8ffdac3724a3e0e6
gnba <gnba@BRPC003418.gft.com>
2019-07-10 11:12:09 -0300

    Atualização do projeto

--- src/main/java/com/lithiumdigital/algamoney/api/resource/CategoriaResource.java
+++ src/main/java/com/lithiumdigital/algamoney/api/resource/CategoriaResource.java
@@ -1,11 +1,19 @@
 package com.lithiumdigital.algamoney.api.resource;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.lithiumdigital.algamoney.api.model.Categoria;
 import com.lithiumdigital.algamoney.api.repository.CategoriaRepository;
@@ -19,7 +27,20 @@ public class CategoriaResource {
 	
 	@GetMapping
 	public List<Categoria> listar(){
+		
 		return categoriaRepository.findAll();
+		
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public void criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+		Categoria categoriaSalva = categoriaRepository.save(categoria);
+		
+		//Pega o codigo e faz a localização da categoria salva para ser recuperada posteriormente
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+				.buildAndExpand(categoriaSalva.getCodigo()).toUri();
+		response.setHeader("Location", uri.toASCIIString());
 		
 	}
 
