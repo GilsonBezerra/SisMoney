cfdfe5a630f81eac189f1180815fd0b93549cb8f
gilsonbezerra <gilson.bezerra@live.com>
2019-09-02 18:41:20 -0300

    Inserção getCauseMessage no Tratamento de Erros

--- src/main/java/com/lithiumdigital/algamoney/api/exceptionhandler/SismoneyExceptionHandler.java
+++ src/main/java/com/lithiumdigital/algamoney/api/exceptionhandler/SismoneyExceptionHandler.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -58,7 +59,7 @@ public class SismoneyExceptionHandler extends ResponseEntityExceptionHandler {
 		@ExceptionHandler({ DataIntegrityViolationException.class })
 		public ResponseEntity<Object>handlerDataIntegrityViolationException(DataIntegrityViolationException ex, WebRequest request){
 			String mensagemUsuario = messageSource.getMessage("recurso.operacao-nao-permitida", null, LocaleContextHolder.getLocale());
-			String mensagemDesenvolvedor = ex.toString();
+			String mensagemDesenvolvedor = ExceptionUtils.getRootCauseMessage(ex);
 			
 			List<ApiErro> erros = Arrays.asList(new ApiErro(mensagemUsuario, mensagemDesenvolvedor));
 			
